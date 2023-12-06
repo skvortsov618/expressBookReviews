99a69ffc31a316ba8e8c50d5be51e138253a13b6
Dmitrii Skvortsov <618skvortsov@gmail.com>
2023-12-05 19:20:13 -0500

    task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
@@ -11,18 +12,71 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+      return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60*60 });
+
+    req.session.authorization = {
+      accessToken,username
+  }
+  return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    //req.user
+    books[isbn]
+    if (friend) { //Check is friend exists
+        let DOB = req.body.DOB;
+        let firstName = req.body.firstName;
+        let lastName = req.body.lastName;
+
+        //if DOB the DOB has been changed, update the DOB 
+        if(DOB) {
+            friend["DOB"] = DOB
+        }
+        //Add similarly for firstName
+        if(firstName) {
+            friend["firstName"] = firstName
+        }
+        //Add similarly for lastName
+        if(lastName) {
+            friend["lastName"] = lastName
+        }
+        friends[email]=friend;
+        res.send(`Friend with the email  ${email} updated.`);
+    }
+    else{
+        res.send("Unable to find friend!");
+    }
 });
 
 module.exports.authenticated = regd_users;
